29dd1bb4af75a3f341080f0eba0f020a0a61ded7
Baris-Yildiz <46918905+Baris-Yildiz@users.noreply.github.com>
2019-08-12 19:36:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Floyds_Triangle_CS
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("How many rows? :");
+
+            int GivenRow = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Environment.NewLine + "Floyd:" + Environment.NewLine);
+
+            int row = 1;
+
+            int number = 1;
+
+            while(row <= GivenRow)
+            {
+                for(int i = 0; i<row; i++)
+                {
+                    Console.Write(number + " ");
+                    number += 1;
+                }
+                Console.WriteLine();
+
+                row += 1;
+            }
+
+            row -= 1;
+
+            number -= 1;
+
+            Console.WriteLine(Environment.NewLine + "Reverse Floyd:" + Environment.NewLine);
+
+            while(row >= 1)
+            {
+                for(int i = 0; i<row; i++)
+                {
+                    Console.Write(number + " ");
+                    number -= 1;
+                }
+
+                Console.WriteLine();
+
+                row -= 1;
+            }
+        }
+    }
+}
